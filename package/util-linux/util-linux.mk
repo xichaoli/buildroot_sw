1b6a6ea8b922f8973e4d17e2d883a6ca5f915b8c
xichao <xichao@sina.cn>
2018-04-23 13:11:02 -0400

    编译util-linux时去掉python支持，另外增加一个补丁，在链接readline时加上curses库

--- package/util-linux/util-linux.mk
+++ package/util-linux/util-linux.mk
@@ -211,17 +211,17 @@ HOST_UTIL_LINUX_CONF_OPTS += --disable-all-programs
 endif
 
 # Install libmount Python bindings
-ifeq ($(BR2_PACKAGE_PYTHON)$(BR2_PACKAGE_PYTHON3),y)
-UTIL_LINUX_CONF_OPTS += --with-python
-UTIL_LINUX_DEPENDENCIES += $(if $(BR2_PACKAGE_PYTHON),python,python3)
-ifeq ($(BR2_PACKAGE_UTIL_LINUX_LIBMOUNT),y)
-UTIL_LINUX_CONF_OPTS += --enable-pylibmount
-else
-UTIL_LINUX_CONF_OPTS += --disable-pylibmount
-endif
-else
+#ifeq ($(BR2_PACKAGE_PYTHON)$(BR2_PACKAGE_PYTHON3),y)
+#UTIL_LINUX_CONF_OPTS += --with-python
+#UTIL_LINUX_DEPENDENCIES += $(if $(BR2_PACKAGE_PYTHON),python,python3)
+#ifeq ($(BR2_PACKAGE_UTIL_LINUX_LIBMOUNT),y)
+#UTIL_LINUX_CONF_OPTS += --enable-pylibmount
+#else
+#UTIL_LINUX_CONF_OPTS += --disable-pylibmount
+#endif
+#else
 UTIL_LINUX_CONF_OPTS += --without-python
-endif
+#endif
 
 ifeq ($(BR2_PACKAGE_READLINE),y)
 UTIL_LINUX_CONF_OPTS += --with-readline

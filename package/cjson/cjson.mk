f7d9e8c9124352330d346af2f9f0c87f3b6dc39d
xichao <xichao@sina.cn>
2018-05-16 11:16:29 +0800

    cjson enable utils

--- package/cjson/cjson.mk
+++ package/cjson/cjson.mk
@@ -9,6 +9,11 @@ CJSON_SITE = $(call github,DaveGamble,cjson,$(CJSON_VERSION))
 CJSON_INSTALL_STAGING = YES
 CJSON_LICENSE = MIT
 CJSON_LICENSE_FILES = LICENSE
+
+# Compile cjson_utils
+CJSON_CONF_OPTS += \
+	-DENABLE_CJSON_UTILS=On
+
 # Set ENABLE_CUSTOM_COMPILER_FLAGS to OFF in particular to disable
 # -fstack-protector-strong which depends on BR2_TOOLCHAIN_HAS_SSP
 CJSON_CONF_OPTS += \
